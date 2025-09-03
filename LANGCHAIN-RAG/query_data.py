b4db4c345d36d9b9bd51f3b41003e5909f61da77
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-09-03 16:15:54 -0400

    generate response

--- LANGCHAIN-RAG/query_data.py
+++ LANGCHAIN-RAG/query_data.py
@@ -3,10 +3,17 @@ import argparse
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
+from langchain.promps import ChatPromptTemplate
 
 CHROMA_PATH = "chroma"
 
+PROMPT_TEMPLATE = """
+Answer the question based only on following context:
+{context}
+---
+Answer the question based on the above context: {question}
 
+"""
 def main():
     # argparse module to create user friendly command line interfaces (CLI)
     parser = argparse.ArgumentParser("simple program to demonstrate query data")
@@ -23,9 +30,23 @@ def main():
     if len(results) == 0 or results[0][1] < 0.7: # or relevance score below certain threshold we define return early
         print(f"Unable to find matching results")
         return
-    # print content for each page
+
+    # now we can feed into open ai 
+    # creates the prompt 
     context_text = "\n\n --- \n\n".join([doc.page_content for doc, _score in results])
-    print(context_text)
+    # print(context_text) # print content for each page
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context= context_text, question = query_text)
+    print(prompt)
+
+    # uses LLM to answer the question 
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in result]
+    formatted_response = f"Response: {response_text}\n Sources: {sources}"
+    print(formatted_response)
+
 
 
 if __name__=="__main__":
