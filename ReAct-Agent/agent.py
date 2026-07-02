21465e8a073f57a38c51bff1756d988125e578ab
Topgyal Gurung <topgyaltsering3@gmail.com>
2026-07-01 23:23:33 -0400

    build agent from scratch

--- ReAct-Agent/agent.py
+++ ReAct-Agent/agent.py
@@ -6,7 +6,7 @@ import httpx
 import os
 from dotenv import load_dotenv
 
-_ = load_dotenv
+_ = load_dotenv()
 
 from openai import OpenAI
 
@@ -18,6 +18,39 @@ client = OpenAI()
 # )
 
 # chat_completion.choices[0].messages.content
+
+prompt = """
+You run in a loop of Thought, Action, PAUSE, Observation.
+At the end of the loop you output an Answer
+Use Thought to describe your thoughts about the question you have been asked.
+Use Action to run one of the actions available to you - then return PAUSE.
+Observation will be the result of running those actions.
+
+Your available actions are:
+
+calculate:
+e.g. calculate: 4 * 7 / 3
+Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
+
+average_dog_weight:
+e.g. average_dog_weight: Collie
+returns average weight of a dog when given the breed
+
+Example session:
+
+Question: How much does a Bulldog weigh?
+Thought: I should look the dogs weight using average_dog_weight
+Action: average_dog_weight: Bulldog
+PAUSE
+
+You will be called again with this:
+
+Observation: A Bulldog weights 51 lbs
+
+You then output:
+
+Answer: A bulldog weights 51 lbs
+""".strip()
 
 class Agent:
     def __init__(self, system=""):
@@ -37,3 +70,64 @@ class Agent:
                         messages=self.messages)
         return completion.choices[0].message.content
 
+
+def calculate(what):
+    return eval(what)
+
+def average_dog_weight(name):
+    if name in "Scottish Terrier": 
+        return("Scottish Terriers average 20 lbs")
+    elif name in "Border Collie":
+        return("a Border Collies average weight is 37 lbs")
+    elif name in "Toy Poodle":
+        return("a toy poodles average weight is 7 lbs")
+    else:
+        return("An average dog weights 50 lbs")
+
+known_actions = {
+    "calculate": calculate,
+    "average_dog_weight": average_dog_weight
+}
+
+action_re = re.compile('^Action: (\w+): (.*)$')   # python regular expression to selection action
+
+def query(question, max_turns=5):
+    i = 0
+    bot = Agent(prompt)
+    next_prompt = question
+    while i < max_turns:
+        i += 1
+        result = bot(next_prompt)
+        print(result)
+        actions = [
+            action_re.match(a) 
+            for a in result.split('\n') 
+            if action_re.match(a)
+        ]
+        if actions:
+            # There is an action to run
+            action, action_input = actions[0].groups()
+            if action not in known_actions:
+                raise Exception("Unknown action: {}: {}".format(action, action_input))
+            print(" -- running {} {}".format(action, action_input))
+            observation = known_actions[action](action_input)
+            print("Observation:", observation)
+            next_prompt = "Observation: {}".format(observation)
+        else:
+            return
+
+def main():
+    # abot = Agent(prompt)
+    # result = abot("How much does a toy poodle weigh")
+    # print(result)
+
+    # result = average_dog_weight("Toy Poodle")
+    # next_prompt = "Observation: {}".format(result)
+    # abot(next_prompt)
+    # abot.messages
+
+    question = """I have 2 dogs, a border collie and a scottish terrier. What is their combined weight"""
+    query(question)
+
+if __name__ == "__main__":
+    main()
