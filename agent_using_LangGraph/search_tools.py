1b297fb1de0e3b14358cce6fa5aaa82afce907e4
Topgyal Gurung <topgyaltsering3@gmail.com>
2026-07-02 14:38:10 -0400

    persistence and streaming

--- agent_using_LangGraph/search_tools.py
+++ agent_using_LangGraph/search_tools.py
@@ -1,8 +1,21 @@
+# libraries
 from dotenv import load_dotenv
-import os 
+import os
 from tavily import TavilyClient
 
+# load environment variables from .env file
 _ = load_dotenv()
 
-client = TavilyClient(apikey=os.environ.get("TAVILY_API_KEY"))
+# connect
+# TAVILY API Researcher free 1000 api credit/month
+client = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
+
+# run search
+result = client.search("What is in Nvidia's new Blackwell GPU?",
+                       include_answer=True)
+
+# print the answer
+print(result["answer"])
+
+
 
